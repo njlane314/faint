7aada219aec9baea7e7a71fc46b7fe5767fa42ee
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-09-29 13:17:34 +0100

    Refactor selection processors into compiled library

---
+++ src/rarexsecLinkDef.h
@@ -0,0 +1,12 @@
+#include "rarexsec/EventProcessor.h"
+#include "rarexsec/MuonSelector.h"
+#include "rarexsec/PreSelection.h"
+#include "rarexsec/TruthClassifier.h"
+
+#ifdef __CLING__
+#pragma link C++ namespace analysis;
+#pragma link C++ class analysis::EventProcessor+;
+#pragma link C++ class analysis::PreSelection+;
+#pragma link C++ class analysis::MuonSelector+;
+#pragma link C++ class analysis::TruthClassifier+;
+#endif
