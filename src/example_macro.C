ac20e5f3117f8afa48004efb499bec23065b8a06
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-09-30 12:40:33 +0100

    Refactor campaign interface into dataset

--- src/example_macro.C
+++ src/example_macro.C
@@ -1,45 +1,33 @@
+#include <ROOT/RDataFrame.hxx>
+#include <faint/Dataset.h>
+#include <faint/Log.h>
+
 #include <iostream>
-#include <stdexcept>
-#include <string>
-#include <vector>
 
-#include "TSystem.h"
-#include "ROOT/RDataFrame.hxx"
+int main(int argc, char** argv) {
+  faint::log::init();
+  faint::log::set_level(faint::log::Level::kDebug);
 
-#include <faint/Campaign.h>
-
-void example_macro(const char* run_config = nullptr)
-{
-  if (gSystem->Load("libfaint_root")) {
-    throw std::runtime_error("Failed to load libfaint_root library");
+  std::string config_path;
+  if (argc > 1) {
+    config_path = argv[1];
+  } else {
+    config_path = faint::dataset::run_config_path();
   }
 
-  std::string config_path = run_config ? run_config : "";
-  if (config_path.empty()) {
-    config_path = faint::campaign::run_config_path();
+  faint::dataset::Options options;
+  options.ntuple_dir = faint::dataset::ntuple_directory(config_path);
+  auto dataset = faint::dataset::Dataset::open(config_path, options);
+
+  auto keys = dataset.sample_keys();
+  std::cout << "Loaded the following samples:\n";
+  for (const auto& key : keys) {
+    std::cout << "  - " << key << '\n';
   }
 
-  ROOT::EnableImplicitMT();
+  auto df = dataset.df(keys.at(0));
+  auto count = df.Count();
+  std::cout << "Count of rows: " << *count << '\n';
 
-  faint::campaign::Options options;
-  options.beam = "numi-fhc";
-  options.periods = {"run1"};
-  options.ntuple_dir = faint::campaign::ntuple_directory(config_path);
-
-  auto campaign = faint::campaign::Campaign::open(config_path, options);
-
-  std::cout << "Loaded beam " << campaign.beam() << " for";
-  for (const auto& p : campaign.periods()) {
-    std::cout << ' ' << p;
-  }
-  std::cout << " with " << campaign.sample_keys().size() << " samples." << std::endl;
-
-  for (const auto& key : campaign.sample_keys()) {
-    auto final_count = campaign.final(key).Count();
-    auto final_count_value = final_count.GetValue();
-    std::cout << "Final selection entries for " << key << ": " << final_count_value << std::endl;
-  }
-
-  std::cout << "Total POT: " << campaign.pot() << std::endl;
-  std::cout << "Total triggers: " << campaign.triggers() << std::endl;
+  return 0;
 }
