9d2ecbdb1300f1db94a7c7ff64c0c2f012e3068d
Nicholas Lane <nicholas.lane@postgrad.manchester.ac.uk>
2025-10-02 12:17:07 -0500

    Update

--- rarexsec-root.sh
+++ rarexsec-root.sh
@@ -16,16 +16,9 @@ if [ -n "${JSON_INC_PATH}" ]; then
   export ROOT_INCLUDE_PATH="${JSON_INC_PATH}:${ROOT_INCLUDE_PATH}"
 fi
 
-case "$(uname -s)" in
-  Darwin) export DYLD_LIBRARY_PATH="${LIBDIR}:${DYLD_LIBRARY_PATH}" ;;
-  *)      export LD_LIBRARY_PATH="${LIBDIR}:${LD_LIBRARY_PATH}" ;;
-esac
+export LD_LIBRARY_PATH="${LIBDIR}:${LD_LIBRARY_PATH}"
 export ROOT_INCLUDE_PATH="${INCDIR}:${ROOT_INCLUDE_PATH}"
 
-if [ "$(uname -s)" = "Darwin" ]; then
-  LIBEXT="dylib"
-else
-  LIBEXT="so"
-fi
+LIBEXT="so"
 
 root -l -q -e "gROOT->LoadMacro(\"${MACRO}\"); setup_rarexsec(\"${LIBDIR}/librarexsec.${LIBEXT}\",\"${INCDIR}\");" "$@"
