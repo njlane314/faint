a0045f6c7872dba1568a214820bff6a993e9a142
Nicholas <107313324+njlane314@users.noreply.github.com>
2025-10-11 17:21:40 +0100

    Refactor setup_rarexsec function for clarity

--- scripts/setup_rarexsec.C
+++ scripts/setup_rarexsec.C
@@ -4,13 +4,14 @@
 #include <string>
 
 void setup_rarexsec(const char* libpath, const char* incdir) {
-  if (libpath && *libpath) {
-    int s = gSystem->Load(libpath);
-    if (s < 0) std::fprintf(stderr, "setup_rarexsec: failed to load %s (%d)\n", libpath, s);
-  }
-  if (incdir && *incdir) {
-    gInterpreter->AddIncludePath(incdir);
-    std::string sub = std::string(incdir) + "/rarexsec";
-    if (!gSystem->AccessPathName(sub.c_str())) gInterpreter->AddIncludePath(sub.c_str());
-  }
+    if (libpath && *libpath) {
+        int s = gSystem->Load(libpath);
+        if (s < 0) std::fprintf(stderr, "setup_rarexsec: failed to load %s (%d)\n", libpath, s);
+    }
+
+    if (incdir && *incdir) {
+        gInterpreter->AddIncludePath(incdir);
+        std::string sub = std::string(incdir) + "/rarexsec";
+        if (!gSystem->AccessPathName(sub.c_str())) gInterpreter->AddIncludePath(sub.c_str());
+    }
 }
